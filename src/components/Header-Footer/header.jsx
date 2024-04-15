34a45f6b9525df96c78d5271a7567c5cfd238bec
Divij Mahajan <d.mahajan2004@gmail.com>
2024-04-16 00:02:01 +0530

    last commit

--- src/components/Header-Footer/header.jsx
+++ src/components/Header-Footer/header.jsx
@@ -22,6 +22,15 @@ const LogoutButton = () => {
 function Header() {
     const navigate = new useNavigate()
     const { user, isAuthenticated, isLoading } = useAuth0();
+    let categories = [
+        "Sports",
+        "Movies",
+        "Hotels",
+        "Airlines",
+        "Trains",
+        "Bus",
+        "Concerts",
+    ]
     return (
         <div className="fixed top-0 left-0  h-24 z-50 p-7 flex justify-between header-div" style={{ width: "100vw" }}>
             <div className="flex items-center">
@@ -30,6 +39,11 @@ function Header() {
                 <div className="text-4xl font-black ml-4" onClick={() => { navigate("/") }}>
                     Ticket Hive
                 </div>
+            </div>
+            <div className="flex items-center gap-5 text-xl">
+                {categories.map((category, i) => {
+                    return <div className="hover:scale-110" onClick={() => { navigate(`/${category}`) }}>{category}</div>
+                })}
             </div>
             <div className="text-2xl mr-4 flex items-center gap-10">
                 {(isAuthenticated) ?
